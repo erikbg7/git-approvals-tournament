4408d1ccba7bb7e55a4d2be1f5cbf2a7ffdf55f1
erikblanca <erik.bg7@gmail.com>
2022-06-27 19:35:57 +0200

    New logo for Github and Gitlab tournament

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,19 +5,18 @@ import {
   Avatar,
   HStack,
   Link,
-  IconButton,
   Button,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   MenuDivider,
-  Stack,
   useDisclosure,
 } from '@chakra-ui/react';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+import { AddIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
+import Image from 'next/image';
 
 const NavSignButton = () => {
   const { data: session, status } = useSession();
@@ -68,48 +67,10 @@ const NavSignButton = () => {
       size={'md'}
       mr={4}
       leftIcon={<AddIcon />}
-      onClick={() => signIn('gitlab', { callbackUrl: '/tournament' })}
+      onClick={() => signIn('gitlab', { callbackUrl: '/tournament/gitlab' })}
     >
       Sign In with Gitlab
     </Button>
-  );
-};
-
-const ClipLogo = () => {
-  return (
-    <svg width="42" height="42" className="tanuki-logo" viewBox="0 0 36 36">
-      <path
-        className="tanuki-shape tanuki-left-ear"
-        fill="#e24329"
-        d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"
-      />
-      <path
-        className="tanuki-shape tanuki-right-ear"
-        fill="#e24329"
-        d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"
-      />
-      <path className="tanuki-shape tanuki-nose" fill="#e24329" d="M18,34.38 3,14 33,14 Z" />
-      <path
-        className="tanuki-shape tanuki-left-eye"
-        fill="#fc6d26"
-        d="M18,34.38 11.38,14 2,14 6,25Z"
-      />
-      <path
-        className="tanuki-shape tanuki-right-eye"
-        fill="#fc6d26"
-        d="M18,34.38 24.62,14 34,14 30,25Z"
-      />
-      <path
-        className="tanuki-shape tanuki-left-cheek"
-        fill="#fca326"
-        d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"
-      />
-      <path
-        className="tanuki-shape tanuki-right-cheek"
-        fill="#fca326"
-        d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"
-      />
-    </svg>
   );
 };
 
@@ -123,11 +84,17 @@ const NavBar = ({ children }: { children: ReactNode }) => {
           <HStack spacing={8} alignItems={'center'}>
             <NextLink href={'/'}>
               <Box>
-                <ClipLogo />
+                <Image src="/logo.svg" alt="logo" width="44" height="44" />
               </Box>
             </NextLink>
             <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-              <Link as={'a'}> by Erik Blanca </Link>
+              <Link
+                as={'a'}
+                href={'https://www.linkedin.com/in/erik-blanca-gomez-32455a162/'}
+                isExternal
+              >
+                by Erik Blanca
+              </Link>
             </HStack>
           </HStack>
           <Flex alignItems={'center'}>
