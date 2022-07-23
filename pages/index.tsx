a76284a445819d06334f2363c014a1ecf3438fc6
erikbg7 <erik.bg7@gmail.com>
2022-07-23 15:41:03 +0200

    Move Landing to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { Button, Text, Heading, VStack, HStack } from '@chakra-ui/react';
-import { signIn } from 'next-auth/react';
+import { Landing } from '../components/Landing';
 
 const Home: NextPage = () => {
   return (
-    <VStack height={'100vh'} display={'flex'} alignItems={'center'} justifyContent={'start'} p={6}>
-      <HStack my={10}>
-        <Text as={'p'} fontSize={'7xl'}>
-          🏆
-        </Text>
-        <Heading as={'h1'} textAlign={'center'}>
-          <Text>Welcome to the Gitalb</Text>
-          <Text>approvers tournament</Text>
-        </Heading>
-        <Text as={'p'} fontSize={'7xl'}>
-          🏆
-        </Text>
-      </HStack>
-      <VStack maxW={'2xl'} bgColor={'whiteAlpha.50'} p={6} borderRadius={8}>
-        <Heading as={'h2'} size={'md'}>
-          What is this?
-        </Heading>
-        <br />
-        <Text fontSize={'lg'} color={'whiteAlpha.700'}>
-          This is a minigame from the company that aims to increase the motivation of the team to
-          review the open PRs.
-        </Text>
-        <Text fontSize={'lg'} color={'whiteAlpha.700'}>
-          In order to achieve it, we will check who is the member of the team that approves and code
-          reviews less, and find a funny punishment for him or her.
-        </Text>
-        <br />
-        <Button
-          variant={'outline'}
-          borderColor={'#fc6d26'}
-          onClick={() => signIn('gitlab', { callbackUrl: '/tournament/gitlab' })}
-        >
-          Sign in with Gitlab
-        </Button>
-        <Button
-          variant={'outline'}
-          borderColor={'gray.400'}
-          onClick={() => signIn('github', { callbackUrl: '/tournament/github' })}
-        >
-          Sign in with Github
-        </Button>
-      </VStack>
-    </VStack>
+    <>
+      {/*Add metadata to head*/}
+      {/*<AppHead/>*/}
+      <Landing />
+    </>
   );
 };
 
