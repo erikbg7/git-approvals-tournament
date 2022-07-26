ab88fa0ee447cbf17db18ba5912f597dc74990e3
erikbg7 <erik.bg7@gmail.com>
2022-07-26 14:44:51 +0200

    Add next config comment to inspect bundles

--- next.config.js
+++ next.config.js
@@ -5,9 +5,23 @@ const nextConfig = {
     domains: ['avatars.githubusercontent.com', 'secure.gravatar.com', 'gitlab.com'],
   },
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
+
+/*
+const nextConfigDev = {
+  productionBrowserSourceMaps: true, // Add to see source maps in Lighthouse
+  ...nextConfig
+}
+
+// npm i @next/bundle-analyzer, then export it in next.config
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer(nextConfigDev);
+*/
 
 module.exports = nextConfig;
